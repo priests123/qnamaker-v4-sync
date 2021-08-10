12fd677381aacf02c134241d20ce0c6e2096668e
Mike Priest <mikepriest123@yahoo.co.uk>
2021-08-10 07:43:07 +0100

    Adding Mockhttpclienthandler

---
+++ QnAMaker.V4.Sync.Tests/Mocks/MockHttpMessageHandler.cs
@@ -0,0 +1,68 @@
+﻿using Moq;
+using Moq.Protected;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QnAMaker.V4.Sync.Tests.Mocks
+{
+    public class MockHttpMessageHandler : Mock<HttpMessageHandler>
+    {
+        public MockHttpMessageHandler MockSendAsyncReturnsOkRequest()
+        {
+            var responseMessage = new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK
+            };
+
+            MockSendAsyncWithContent(responseMessage);
+            return this;
+        }
+
+        public MockHttpMessageHandler MockSendAsyncWithContentReturnsOkRequest(string content)
+        {
+            var responseMessage = new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(content)
+            };
+
+            MockSendAsyncWithContent(responseMessage);
+            return this;
+        }
+
+        public MockHttpMessageHandler MockSendAsyncReturnsBadRequest()
+        {
+            var responseMessage = new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.BadRequest
+            };
+
+            MockSendAsyncWithContent(responseMessage);
+            return this;
+        }
+
+        public MockHttpMessageHandler MockSendAsyncWithContentReturnsBadRequest(string content)
+        {
+            var responseMessage = new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+                Content = new StringContent(content)
+            };
+
+            MockSendAsyncWithContent(responseMessage);
+            return this;
+        }
+
+        private MockHttpMessageHandler MockSendAsyncWithContent(HttpResponseMessage responseMessage)
+        {
+            this.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(responseMessage)
+                .Verifiable();
+
+            return this;
+        }
+    }
+}
